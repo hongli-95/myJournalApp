5640e80639cf537ad1d93c89ad166b601065079f
Hong <joeleehong@gmail.com>
2024-08-12 22:21:32 -0700

    adjusted button position on top bar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -16,7 +16,7 @@ export default async function TopBar() {
 			{isLoggedIn ? (
 				<Link
 					href="/entries"
-					className="flex justify-center items-center bg-transparent border-2 border-transparent p-2 rounded-lg text-white 
+					className="flex justify-center items-center bg-transparent border-2 border-transparent p-1 my-1 rounded-lg md:flex-none flex-1 text-white 
         hover:border-white hover:scale-105 hover:shadow-md  
           focus-visible:border-white focus-visible:shadow-md 
         active:scale-95 transition-all"
@@ -36,7 +36,7 @@ export default async function TopBar() {
 
 			{isLoggedIn ? (
 				<LogoutLink
-					className="bg-transparent border-2 border-transparent p-2 rounded-lg text-white 
+					className="flex justify-center items-center bg-transparent border-2 border-transparent p-1 rounded-lg  text-white 
         hover:border-white hover:scale-105 hover:shadow-md transition-all 
           focus-visible:border-white focus-visible:shadow-md 
         active:scale-95"
